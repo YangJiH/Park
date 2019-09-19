44bde36928726bbe2521f4bf596978b9f85904c7
unknown <dnflwlqdml95@naver.com>
2019-09-20 04:50:51 +0900

    Initial commit

---
+++ Source/Park/ParkGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ParkGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AParkGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AParkGameMode();
+};
+
+
+
